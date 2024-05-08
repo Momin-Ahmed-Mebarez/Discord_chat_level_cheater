e8295e9dc25c4037e1d6c95ff0a3ae0ff7d20ae4
Momin-Ahmed-Mebarez <momenmomen35@gmail.com>
2024-05-08 15:59:48 +0300

    Wrong file uploaded

--- Lvl spammer.py
+++ Lvl spammer.py
@@ -2,121 +2,53 @@ import pyautogui
 import pyperclip
 import time
 import random
-line_count = 0
-msg_count = 0
-
-with open("Arabic.txt", "r", encoding='utf-8') as read_file:
-    word = read_file.readlines()
-    read_file.close()
-
-for line in word:
-        if line != "\n":
-            line_count += 1
-
-pyautogui.click(1120,677)
-while True:
-    pyperclip.copy(word[random.randint(0, line_count - 1)])
-    pyautogui.hotkey("ctrl","v")
-    pyautogui.press("enter")
-    time.sleep(1)
-    pyautogui.click(1128,619)
-    pyautogui.click(1311,590)
-#    time.sleep(1)
-    pyautogui.click(1140,640)
-    time.sleep(1)
-    pyautogui.click(1172,500)
-    msg_count = msg_count + 1
-    print(msg_count)
-    time.sleep(1)
-    pyautogui.hotkey("ctrl","pgdn")
-    time.sleep(57)
-    pyautogui.hotkey("ctrl","pgdn")
-    time.sleep(1)    
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-old numbers
-time.sleep(1)
-time.sleep(1)        
-time.sleep(1)
-time.sleep(27)
-time.sleep(1)
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-q_cd = 0
-w_cd = 0
-
-pyautogui.click(841,352)
-while True:
-    if w_cd <= 0:
-        pyautogui.press('w')
-        w_cd = 27
-        
-    if q_cd <= 0:
-        pyautogui.press('q')
-        q_cd = 9
-        w_cd = w_cd - 1
-    pyautogui.click(841,352)
-    time.sleep(1)
-    q_cd = q_cd - 1
-    w_cd = w_cd - 1
-
+def start():
+    line_count = 0
+    msg_count = 0
+    #A timer to give a chance to open the page
+    time.sleep(2)
     
-#Q = 9
-#W = 27
-"""
+    #Reading the list file
+    with open("Arabic.txt", "r", encoding='utf-8') as read_file:
+        word = read_file.readlines()
+        read_file.close()
+        
+    #Getting the number of lines in the list
+    for line in word:
+            if line != "\n":
+                line_count += 1
+                
+    #Clicking on page to get focus (The number need to be changed depending on your screen (x,y))
+    pyautogui.click(1120,677)
+    
+    while True:
+        
+        #Chosing a random word to put in clipboard
+        pyperclip.copy(word[random.randint(0, line_count - 1)])
+
+        #Sending the word 
+        pyautogui.hotkey("ctrl","v")
+        pyautogui.press("enter")
+
+        #A timer to avoid beign flagged as spam (the number can be lowerd)
+        time.sleep(57)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    start()
+
+
+
+
+
+
+
+
+
